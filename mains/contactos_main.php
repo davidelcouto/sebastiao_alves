9d037e269eb0de646a6ea3d9946fcfa891780df8
davidelcouto <davidelcouto@gmail.com>
2023-12-27 13:33:04 +0000

    modified:   css/style.css 	modified:   mains/contactos_main.php 	modified:   mains/imprensa_main.php 	modified:   mains/livro_main.php 	modified:   maquetes/autor.html 	new file:   maquetes/contactos.html 	new file:   maquetes/imprensa.html 	modified:   maquetes/index.html 	new file:   maquetes/livro1.html 	new file:   maquetes/livro2.html 	new file:   maquetes/livro3.html 	new file:   maquetes/livro4.html

--- mains/contactos_main.php
+++ mains/contactos_main.php
@@ -1,72 +1,72 @@
 <main class="container-fluid px-0 foco" id="main">
-            <div class="row caixaautor2">
-                <div class="col-10 textocaixa ms-5">
-                    <div class="row">
-                        <div class="col novidade">contactos</div>                        
-                    </div>
-                    <div class="row">
-                        <div class="col t1 pb-3">pode contactar-me atraves do e-mail ou telefone</div>                       
-                    </div>
+    <div class="row caixaautor2">
+        <div class="col-10 textocaixa ms-5">
+            <div class="row">
+                <div class="col novidade">contactos</div>                        
+            </div>
+            <div class="row">
+                <div class="col t1 pb-3">pode contactar-me atraves do e-mail ou telefone</div>                       
+            </div>
+        </div>
+    </div>
+
+    <div class="row mx-0 d-flex justify-content-center text-center mt-5 d-md-block d-none">
+        <div class="col-12 contactos">
+            <div class="row mt-5 d-flex justify-content-center text-center">
+                <div class="col-3 contato_titulo2">Telefone</div>
+                <div class="col-2 ms-5 contato_titulo2">morada</div>
+                <div class="col-3 ms-5 contato_titulo2">e-mail</div>
+            </div>
+            <div class="row mt-1 d-flex justify-content-center text-center">
+                <div class="col-3 contato_conteudo"><?= getContactos()["telefone"] ?></div>
+                <div class="col-3 contato_conteudo"><?= getContactos()["morada"] ?></div>
+                <div class="col-3 contato_conteudo"><?= getContactos()["mail"] ?></div>
+            </div>                       
+        </div>
+
+        <div class="row d-flex justify-content-center text-center">
+            <div class="col-1 linha4 mt-5 mb-2 ms-3 d-flex"></div>
+        </div>
+
+        <div class="row d-flex justify-content-center text-center">
+            <div class="col-12">
+                <div class="row mt-5 d-flex justify-content-center text-center">
+                    <div class="col-3 ms-2 contato_titulo2">Horário</div>
                 </div>
+                <div class="row mt-1 d-flex justify-content-center text-center mb-5">
+                    <div class="col-3 ms-2 contato_conteudo"><?= getContactos()["horario"] ?></div>
+                </div>                       
+            </div>
+        </div>
+    </div>
+
+    <!--MOBILE-->
+
+    <div class="row d-block d-md-none mt-5 mx-auto">
+        <div class="row d-flex justify-content-center text-center mt-5">
+            <div class="row mt-3 d-flex justify-content-center text-center">
+                <div class="col-8 contato_titulo2">telefone</div>
+                <div class="col-8 mt-1 contato_conteudo"><?= getContactos()["telefone"] ?></div>
+                <div class="col-8 mt-4 contato_titulo2 pt-4">morada</div>
+                <div class="col-8 mt-1 contato_conteudo"><?= getContactos()["morada"] ?></div>
+                <div class="col-8 mt-4 contato_titulo2 pt-4">e-mail</div>
+                <div class="col-8 mt-1 contato_conteudo"><?= getContactos()["mail"] ?></div>
             </div>
 
-            <div class="row mx-0 d-flex justify-content-center text-center mt-5 d-md-block d-none">
-                <div class="col-12 contactos">
+            <div class="row d-flex justify-content-center text-center">
+                <div class="col-1 traco3 mt-5 mb-2 mx-5 d-flex"></div>
+            </div>
+
+            <div class="row d-flex justify-content-center text-center">
+                <div class="col-12">
                     <div class="row mt-5 d-flex justify-content-center text-center">
-                        <div class="col-3 contato_titulo2">Telefone</div>
-                        <div class="col-2 ms-5 contato_titulo2">morada</div>
-                        <div class="col-3 ms-5 contato_titulo2">e-mail</div>
+                        <div class="col-3 ms-2 contato_titulo2">Horário</div>
                     </div>
-                    <div class="row mt-1 d-flex justify-content-center text-center">
-                        <div class="col-3 contato_conteudo"><?= getContactos()["telefone"] ?></div>
-                        <div class="col-3 contato_conteudo"><?= getContactos()["morada"] ?></div>
-                        <div class="col-3 contato_conteudo"><?= getContactos()["mail"] ?></div>
+                    <div class="row mt-1 d-flex justify-content-center text-center mb-5">
+                        <div class="col-8 ms-2 contato_conteudo mb-4"><?= getContactos()["horario"] ?></div>
                     </div>                       
                 </div>
-
-                <div class="row d-flex justify-content-center text-center">
-                    <div class="col-1 linha4 mt-5 mb-2 ms-3 d-flex"></div>
-                </div>
-
-                <div class="row d-flex justify-content-center text-center">
-                    <div class="col-12">
-                        <div class="row mt-5 d-flex justify-content-center text-center">
-                            <div class="col-3 ms-2 contato_titulo2">Horário</div>
-                        </div>
-                        <div class="row mt-1 d-flex justify-content-center text-center mb-5">
-                            <div class="col-3 ms-2 contato_conteudo"><?= getContactos()["horario"] ?></div>
-                        </div>                       
-                    </div>
-                </div>
-            </div>
-
-            <!--MOBILE-->
-
-            <div class="row d-block d-md-none mt-5 mx-auto">
-                <div class="row d-flex justify-content-center text-center mt-5">
-                    <div class="row mt-3 d-flex justify-content-center text-center">
-                        <div class="col-8 contato_titulo2">telefone</div>
-                        <div class="col-8 mt-1 contato_conteudo"><?= getContactos()["telefone"] ?></div>
-                        <div class="col-8 mt-4 contato_titulo2 pt-4">morada</div>
-                        <div class="col-8 mt-1 contato_conteudo"><?= getContactos()["morada"] ?></div>
-                        <div class="col-8 mt-4 contato_titulo2 pt-4">e-mail</div>
-                        <div class="col-8 mt-1 contato_conteudo"><?= getContactos()["mail"] ?></div>
-                    </div>
-
-                    <div class="row d-flex justify-content-center text-center">
-                        <div class="col-1 traco3 mt-5 mb-2 mx-5 d-flex"></div>
-                    </div>
-    
-                    <div class="row d-flex justify-content-center text-center">
-                        <div class="col-12">
-                            <div class="row mt-5 d-flex justify-content-center text-center">
-                                <div class="col-3 ms-2 contato_titulo2">Horário</div>
-                            </div>
-                            <div class="row mt-1 d-flex justify-content-center text-center mb-5">
-                                <div class="col-8 ms-2 contato_conteudo mb-4"><?= getContactos()["horario"] ?></div>
-                            </div>                       
-                        </div>
-                    </div> 
-                </div>
-            </div>
-       </main>  
+            </div> 
+        </div>
+    </div>
+</main>  
--- mains/imprensa_main.php
+++ mains/imprensa_main.php
@@ -21,72 +21,72 @@ $pagina_atual = array_slice($publi, ($numero_pagina-1)*$elementos_por_pagina, $e
 ?>
 
 <main class="px-0 mx-0 container-fluid foco">
-            <div class="row caixaautor2">
-                <div class="col-10 textocaixa ms-5">
-                    <div class="row">
-                        <div class="col novidade">imprensa</div>
-                    </div>
-                    <div class="row">
-                        <div class="col t1">últimas notícias</div>
-                    </div>
+    <div class="row caixaautor2">
+        <div class="col-10 textocaixa ms-5">
+            <div class="row">
+                <div class="col novidade">imprensa</div>
+            </div>
+            <div class="row">
+                <div class="col t1">últimas notícias</div>
+            </div>
+        </div>
+    </div>
+
+    <?php foreach($pagina_atual as $p): ?>
+    <div class="row m-0 imprensa-caixa d-flex mt-3 justify-content-center">
+        <div class="col-10 m-0 p-0">
+            <div class="row m-0 p-0 pb-2 t1">
+                <?= $p["titulo"]; ?>
+            </div>
+            <div class="row m-0 p-0">
+                <div class="col-12 linha3 d-flex"></div>
+            </div>
+            <div class="row m-4 p-0 d-flex mt-3 justify-content-end imprensa">
+                <?= $p["data_pub"]; ?>
+            </div>
+            <div class="row m-0 p-0 pt-1">
+                <img class=" imprensa-img" src="<?= $p["imagem"]; ?>" alt="">
+            </div>
+            <div class="row m-0 p-0 d-flex flex-row justify-content-end">
+                <div class="col-12 p m-0  pt-4">
+                    <?= $p["texto"]; ?>
                 </div>
             </div>
+        </div>
+    </div>
+    <?php endforeach; ?>
 
-            <?php foreach($pagina_atual as $p): ?>
-            <div class="row m-0 imprensa-caixa d-flex mt-3 justify-content-center">
-                <div class="col-10 m-0 p-0">
-                    <div class="row m-0 p-0 pb-2 t1">
-                        <?= $p["titulo"]; ?>
-                    </div>
-                    <div class="row m-0 p-0">
-                        <div class="col-12 linha3 d-flex"></div>
-                    </div>
-                    <div class="row m-4 p-0 d-flex mt-3 justify-content-end imprensa">
-                        <?= $p["data_pub"]; ?>
-                    </div>
-                    <div class="row m-0 p-0 pt-1">
-                        <img class=" imprensa-img" src="<?= $p["imagem"]; ?>" alt="">
-                    </div>
-                    <div class="row m-0 p-0 d-flex flex-row justify-content-end">
-                        <div class="col-12 p m-0  pt-4">
-                            <?= $p["texto"]; ?>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <?php endforeach; ?>
+    <br><br>
 
-            <br><br>
+    <div class="col-12 d-flex justify-content-center">
+        <form action="">   
+            <button class="voltar-pagina" type="submit" name="numero_pagina" value="<?= $numero_pagina-1 ?>" <?= ($numero_pagina == 1) ? "disabled" : ""; ?>></button>
 
-            <div class="col-12 d-flex justify-content-center">
-                <form action="">   
-                    <button class="voltar-pagina" type="submit" name="numero_pagina" value="<?= $numero_pagina-1 ?>" <?= ($numero_pagina == 1) ? "disabled" : ""; ?>></button>
+            <?php 
 
-                    <?php 
+            $start = $numero_pagina - 2;
+            $end = $start + 5;
 
-                    $start = $numero_pagina - 2;
-                    $end = $start + 5;
+            ?>
 
-                    ?>
+            <?php if($start > 1): ?>
+                <span>...</span>
+            <?php endif; ?>
 
-                    <?php if($start > 1): ?>
-                        <span>...</span>
-                    <?php endif; ?>
+            <?php for($i=$start; $i<$end; $i++): ?>
 
-                    <?php for($i=$start; $i<$end; $i++): ?>
+                <?php if($i >= 1 && $i <= $numero_total_paginas): ?>
+                    <input type="submit" class="<?= ($numero_pagina == $i) ? "active" : ""; ?>" name="numero_pagina" value="<?= $i ?>">
+                <?php endif; ?>
 
-                        <?php if($i >= 1 && $i <= $numero_total_paginas): ?>
-                            <input type="submit" class="<?= ($numero_pagina == $i) ? "active" : ""; ?>" name="numero_pagina" value="<?= $i ?>">
-                        <?php endif; ?>
+            <?php endfor; ?>
 
-                    <?php endfor; ?>
+            <?php if($end <= $numero_total_paginas): ?>
+                <span>...</span>
+            <?php endif; ?>
+            
+            <button class="avancar-pagina" type="submit" name="numero_pagina" value="<?= $numero_pagina+1 ?>" <?= ($numero_pagina == $numero_total_paginas) ? "disabled" : ""; ?>></button>
+        </form>
+    </div>
 
-                    <?php if($end <= $numero_total_paginas): ?>
-                        <span>...</span>
-                    <?php endif; ?>
-                    
-                    <button class="avancar-pagina" type="submit" name="numero_pagina" value="<?= $numero_pagina+1 ?>" <?= ($numero_pagina == $numero_total_paginas) ? "disabled" : ""; ?>></button>
-                </form>
-            </div>
-
-        </main>
+</main>
